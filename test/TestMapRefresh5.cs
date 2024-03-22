f84dda52f4c2d097f730a0541585fdf9d33aa5d0
Michael Kent <68415118+mkent-at-loginvsi@users.noreply.github.com>
2024-03-21 21:38:57 -0700

    add timer test

---
+++ test/TestMapRefresh5.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using LoginPI.Engine.ScriptBase;
+using LoginPI.Engine.ScriptBase.Components;
+public class ArcGISPro : ScriptBase
+{
+    void Execute() 
+    {
+        CreateTimer("OpenMainWindow");
+    }
+}
