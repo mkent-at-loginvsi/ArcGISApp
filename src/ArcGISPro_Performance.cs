b72bdc0b4ffec166adafe14c4ff3eba33738ee48
Michael Kent <68415118+mkent-at-loginvsi@users.noreply.github.com>
2024-04-17 16:01:37 -0700

    Create ArcGISPro_Performance.cs

---
+++ src/ArcGISPro_Performance.cs
@@ -0,0 +1,16 @@
+// TARGET:C:\Program Files\ArcGIS\Pro\bin\ArcGISPro.exe
+// START_IN:C:\Program Files\ArcGIS\Pro\bin\
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using LoginPI.Engine.ScriptBase;
+
+public class ArcGISPro : ScriptBase
+{
+    void Execute() 
+    {
+        
+
+
+    }
+}
